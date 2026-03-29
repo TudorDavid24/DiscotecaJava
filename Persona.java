867dddf1fc429df24b0d39b4cb9677d4cdb6736e
TUDOR-PC\tudor <tudor293david@gmail.com>
2026-03-29 14:17:52 +0200

    Aggiunta metodi che fanno scegliere alla persona cosa fare

--- Persona.java
+++ Persona.java
@@ -4,13 +4,16 @@ public class Persona extends Thread {
 
     String nome;
     Boolean tipoBiglietto;
+    Boolean sesso;
     long tMedioDisco;
     int Soldi;
     Random r = new Random();
+    public Boolean inDiscoteca;
 
     public Persona(String nome, int soldi) {
         this.nome = nome;
         this.tipoBiglietto = r.nextBoolean();
+        this.sesso = r.nextBoolean();
         this.tMedioDisco = 0;
         Soldi = r.nextInt(100);
     }
@@ -18,8 +21,38 @@ public class Persona extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            
+            EntraInCoda();
+        while(inDiscoteca){
+            ScegliCosaFare();
         }
     }
+
+    private void EntraInCoda(){
+        if (tipoBiglietto==true) {
+            //Biglietto fila in loco
+            Singleton.getInstance().codaOffilne.add(this);
+        }
+        else{
+            //Biglietto fila online
+            Singleton.getInstance().codaOnline.add(this);
+        }
+    }
+
+    private void ScegliCosaFare(){
+        int numeroCasuale = r.nextInt(100);
+        if (numeroCasuale<=80) {
+            //Continua a ballare
+            return;
+        }
+        else if (numeroCasuale>80 && numeroCasuale<=95) {
+            //Vai a prendere un drink
+            Singleton.getInstance().codaBar.add(this);
+        }
+        else{
+            //Esci dalla discoteca
+            Singleton.getInstance().spazioDiscoteca.remove(this);
+        }
+    }
+
+
 }
